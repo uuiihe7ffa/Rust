a7865c608dac47fba96fb8822b92e063fd5a12a3
uuiihe7ffa <86650955+uuiihe7ffa@users.noreply.github.com>
2023-05-23 20:36:27 +0300

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,22 @@
-use bevy::prelude::*;
+use actix_web::{App, Error, HttpResponse, HttpServer, web};
+use actix_web::http::{StatusCode};
 
-use std::f32::consts::TAU;
 
-pub const HEIGHT: f32 = 720.0;
-pub const WIDTH: f32 = 1280.0;
-
-#[derive(Component)]
-struct Rotatable {
-    speed: f32,
+async fn home() ->Result<HttpResponse, Error>{
+    Ok(
+        HttpResponse::build(StatusCode::OK)
+            .content_type("text/html; charset=utf-8")
+            .body(include_str!("../templates/index.html"))
+    )
 }
 
-fn main() {
-    App::new()
-    .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
-    .add_plugins(DefaultPlugins)
-    .add_startup_system(setup)
-    .add_system(camera_controls)
-    .add_system(rotate_cube)
-    .run();
+#[actix_rt::main]
+async fn main()->std::io::Result<()>{
+    HttpServer::new(move||{
+        App::new()
+            .route("/", web::get().to(home))
+    })
+    .bind("127.0.0.1:8808")?
+    .run()
+    .await
 }
-
-fn setup(
-    mut commands: Commands,
-    ass: Res<AssetServer>,
-) {
-    let my_gltf = ass.load("cube.glb#Scene0");
-    commands.spawn((SceneBundle{
-        scene: my_gltf,
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..Default::default()
-        
-    },
-    Rotatable { speed: 0.3 },
-));
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 1.0, 4.0),
-        
-        ..default()
-    });
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    });
-}
-
-fn camera_controls(
-    keyboard: Res<Input<KeyCode>>,
-    mut camera_query: Query<&mut Transform, With<Camera3d>>,
-    time: Res<Time>,
-) {
-    let mut camera = camera_query.single_mut();
-
-    let mut forward = camera.forward();
-    forward.y = 0.0;
-    forward = forward.normalize();
-
-    let mut left = camera.left();
-    left.y = 0.0;
-    left = left.normalize();
-
-    let mut up = camera.up();
-    up.z = 0.0;
-    up = up.normalize();
-
-    let speed = 3.0;
-    let rotate_speed = 1.0;
-
-    if keyboard.pressed(KeyCode::W) {
-        camera.translation += forward * time.delta_seconds() * speed;
-    }
-    if keyboard.pressed(KeyCode::S) {
-        camera.translation -= forward * time.delta_seconds() * speed;
-    }
-    if keyboard.pressed(KeyCode::A) {
-        camera.translation += left * time.delta_seconds() * speed;
-    }
-    if keyboard.pressed(KeyCode::D) {
-        camera.translation -= left * time.delta_seconds() * speed;
-    }
-    if keyboard.pressed(KeyCode::Q) {
-        camera.rotate_axis(Vec3::Y, rotate_speed * time.delta_seconds())
-    }
-    if keyboard.pressed(KeyCode::E) {
-        camera.rotate_axis(Vec3::Y, -rotate_speed * time.delta_seconds())
-    }
-    if keyboard.pressed(KeyCode::Space){
-        camera.translation += up * time.delta_seconds() * speed;
-    }
-    if keyboard.pressed(KeyCode::LControl){
-        camera.translation -= up * time.delta_seconds() * speed;
-    }
-
-}
-
-fn rotate_cube(mut cubes: Query<(&mut Transform, &Rotatable)>, timer: Res<Time>) {
-    for (mut transform, cube) in &mut cubes {
-
-        transform.rotate_y(cube.speed * TAU * timer.delta_seconds());
-    }
-}
-
-
-
